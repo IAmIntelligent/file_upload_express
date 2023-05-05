5be189c8c090aa11d63da94e1fcc9147f8a370db
nihadsaleem <simplefight90@gmail.com>
2023-05-05 16:17:58 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express")
+const app = express()
+const path = require("path")
+
+const multer = require("multer")
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "./Images/")
+    },
+    filename: (req, file, cb) => {
+        console.log(file)
+        cb(null, Date.now() + path.extname(file.originalname))
+    }
+})
+const upload = multer({ storage: storage })
+
+app.set("view engine", "ejs")
+
+app.get("/upload", (req, res) => {
+    res.render("upload.ejs")
+})
+
+app.post('/upload',upload.single("image"), (req, res) => {
+    res.send("Image Uploaded")
+})
+
+app.listen(3000)
