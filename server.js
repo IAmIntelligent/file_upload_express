9bed418d9aa7a15b6214b926bd53c61d5ea4de58
nihadsaleem <simplefight90@gmail.com>
2023-05-06 11:34:13 +0530

    project used multer npm

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const multer = require("multer")
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, "./Images/")
+        console.log("destination",file)
     },
     filename: (req, file, cb) => {
         console.log(file)
@@ -16,12 +17,15 @@ const upload = multer({ storage: storage })
 
 app.set("view engine", "ejs")
 
+
 app.get("/upload", (req, res) => {
-    res.render("upload.ejs")
+    res.render("upload")
+    console.log("get =  Image uploaded")
 })
 
 app.post('/upload',upload.single("image"), (req, res) => {
     res.send("Image Uploaded")
+    console.log(upload)
 })
 
 app.listen(3000)
